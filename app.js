647a9082b2f41addfef4e12ed5cc4c8d9bfa5659
Aditya Ravichandran <adityaravichandran6@gmail.com>
2020-06-30 17:20:23 +0530

    contact section added

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 1000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
-  
-  TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
+function initMap() {
+  var head = document.getElementsByTagName("head")[0];
+
+  // Save the original method
+  var insertBefore = head.insertBefore;
+
+  // Replace it!
+  head.insertBefore = function (newElement, referenceElement) {
+    if (
+      newElement.href &&
+      newElement.href.indexOf(
+        "https://fonts.googleapis.com/css?family=Roboto"
+      ) === 0
+    ) {
+      return;
     }
-  
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
+
+    insertBefore.call(head, newElement, referenceElement);
   };
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-  };
+
+  var uluru = { lat: 13.052701, lng: 80.235727 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}
